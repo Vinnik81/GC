43bc6b8979a3fd045b27ef38bd3a519d4b16030a
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2022-07-09 22:37:17 +0600

    Nullable

---
+++ Nullable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int @string = 10;
+            //int a = null;
+            //Nullable<int> a = null;
+            //int? a = null;
+            string name = null; //= "Vladimir";
+            string result = name ?? "No name" ?? "Too";
+
+            // Console.WriteLine($"Name состоит из: {result?.Length} ");
+            Console.WriteLine($"Name состоит из: {result.Length} ");
+        }
+    }
+}
